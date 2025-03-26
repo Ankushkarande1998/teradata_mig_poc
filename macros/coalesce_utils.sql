d34fcbd58d77844587e12f0c8f565c08600cd5e0
Ankush Karande <ankushkarande555@gmail.com>
2025-03-26 16:15:53 +0530

    commit for remove colease macros

--- macros/coalesce_utils.sql
+++
@@ -1,9 +0,0 @@
-{% macro get_coalesce(column, default_value) %}
-    {% if target.type == 'redshift' %}
-        COALESCE({{ column }}, {{ default_value }})
-    {% elif target.type == 'databricks' %}
-        NVL({{ column }}, {{ default_value }})
-    {% elif target.type == 'starburst' %}
-        COALESCE({{ column }}, {{ default_value }})
-    {% endif %}
-{% endmacro %}
