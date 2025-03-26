0f4a0e1873af2c6112d530b3a65d913f95ac226a
sreelakshmi <sreddy@firsteigen.com>
2025-03-26 12:47:41 +0530

    Commit for writing databricks iceberg format tables

--- models/marts/CCOM_NBRS_BOUGHT_WITH_USAGE.sql
+++ models/marts/CCOM_NBRS_BOUGHT_WITH_USAGE.sql
@@ -2,11 +2,7 @@
   config(
    materialized='incremental',
    incremental_strategy='merge', 
-   table_format='delta', 
-   table_properties={ 'delta.enableIcebergCompatV2' : 'true',
-                      'delta.universalFormat.enabledFormats' : 'iceberg',
-                      'delta.columnMapping.mode' : 'name'
-                    },
+   table_format='iceberg', 
    unique_key=['CUST_OPER_CD', 'CUST_CTRY_CD', 'MONTHSTART', 'PRODUCT', 'ACCESS_NUMBER'] ,
    post_hook=" {{ drop_tables_by_prefix('q2_')}}"
 ) 
--- models/marts/IBIS_CALL_MAPFULL_CCOM.sql
+++ models/marts/IBIS_CALL_MAPFULL_CCOM.sql
@@ -2,11 +2,7 @@
   config(
    materialized='incremental',
    incremental_strategy='merge', 
-   table_format='delta', 
-   table_properties={'delta.enableIcebergCompatV2' : 'true',
-                      'delta.universalFormat.enabledFormats' : 'iceberg',
-                      'delta.columnMapping.mode' : 'name'
-                    },
+   table_format='iceberg', 
    unique_key=['IN_CDR_FILE_ID','IN_CDR_SERIAL_NR','OUT_CDR_FILE_ID', 'OUT_CDR_SERIAL_NR'] ,
    post_hook=" {{ drop_tables_by_prefix('q1_')}}"
 ) 
